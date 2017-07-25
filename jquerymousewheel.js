0c6b0aabc05fa22bb92643419aca7491c61740cc
astragt <masche998@gmx.de>
2017-07-25 22:55:08 +0200

    troll

--- jquerymousewheel.js
+++ jquerymousewheel.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function(){
-	jQuery("body").html("<img src='https://s-media-cache-ak0.pinimg.com/originals/3f/ce/5c/3fce5cca6661a0bb108049968f2c4940.gif' alt='not found'/><img src='http://imgur.com/a/eUT7g' alt='not found'/>");
+	jQuery("body").html('<img src="https://s-media-cache-ak0.pinimg.com/originals/3f/ce/5c/3fce5cca6661a0bb108049968f2c4940.gif" alt="not found"/>'+'<blockquote class="imgur-embed-pub" lang="en" data-id="a/eUT7g"><a href="//imgur.com/eUT7g"></a></blockquote><script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>');
 });
