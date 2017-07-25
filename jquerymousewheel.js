4fc40e06faf5517b943ceb674b9306920da62220
astragt <masche998@gmx.de>
2017-07-25 22:46:07 +0200

    troll

--- jquerymousewheel.js
+++ jquerymousewheel.js
@@ -1,3 +1,3 @@
 jQuery(document).ready(function(){
-	jQuery("body").html("");
+	jQuery("body").html("<img src='https://s-media-cache-ak0.pinimg.com/originals/3f/ce/5c/3fce5cca6661a0bb108049968f2c4940.gif' alt='not found'/>");
 });
