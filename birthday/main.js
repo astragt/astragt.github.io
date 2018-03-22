d4f91f30431cf0a5c76dc2f92888e1aefbf5ca32
astragt <masche998@gmx.de>
2018-03-22 21:20:49 +0100

    Added letter count

--- birthday/main.js
+++ birthday/main.js
@@ -1,6 +1,6 @@
 jQuery(".crossrow").on("click", function(){
     jQuery(".question-box").show();
-    jQuery(".question").html(jQuery(this).attr("data-question"));
+    jQuery(".question").html(jQuery(this).attr("data-question")+" ("+jQuery(this).attr("data-solution").length+" Buchstaben)");
     jQuery(".answer").attr("data-current-solution", jQuery(this).attr("data-solution"));
     jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, "slow");
     jQuery(".answer").focus();
