a92e2d56185d7a719b1c75a36c85d79fdf7e66c9
astragt <masche998@gmx.de>
2018-03-22 21:44:42 +0100

    added ids to crossrows added more answer js

--- birthday/main.js
+++ birthday/main.js
@@ -1,17 +1,21 @@
 jQuery(".crossrow").on("click", function(){
     jQuery(".question-box").show();
-    jQuery(".question-box").attr("data-current-question", jQuery(this).attr("class"));
+    jQuery(".question-box").attr("data-current-question", jQuery(this).attr("id"));
     jQuery(".question").html(jQuery(this).attr("data-question")+" ("+jQuery(this).attr("data-solution").length+" Buchstaben)");
     jQuery(".answer").attr("data-current-solution", jQuery(this).attr("data-solution"));
     jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, "slow");
     jQuery(".answer").focus();
 });
 jQuery(".answer").on("keydown", function () {
-    jQuery(this).val(jQuery(this).val().substr(0, jQuery(this).attr("data-current-solution").length));
     if(jQuery(this).val().toLowerCase() === jQuery(this).attr("data-current-solution")) {
-        jQuery(".question-box").hide();
         jQuery('html, body').animate({
             scrollTop: jQuery(".crossboard").offset().top
-        }, "slow");
+        }, "slow", function(){
+            jQuery(".question-box").hide();
+        });
+        jQuery(this).val("");
+        jQuery("#"+jQuery(".question-box").attr("data-current-question")+" .crossletter").style("color", "black");
+        jQuery("#"+jQuery(".question-box").attr("data-current-question")+" span").style("opacity", "1");
     }
+    jQuery(this).val(jQuery(this).val().substr(0, jQuery(this).attr("data-current-solution").length));
 });
