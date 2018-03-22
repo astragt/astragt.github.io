40d373e57576fbdc7e22894e8cd5cd84e9076954
astragt <masche998@gmx.de>
2018-03-22 21:29:43 +0100

    Added basic answer js

--- birthday/main.js
+++ birthday/main.js
@@ -1,7 +1,17 @@
 jQuery(".crossrow").on("click", function(){
     jQuery(".question-box").show();
+    jQuery(".question-box").attr("data-current-question", jQuery(this).attr("class"));
     jQuery(".question").html(jQuery(this).attr("data-question")+" ("+jQuery(this).attr("data-solution").length+" Buchstaben)");
     jQuery(".answer").attr("data-current-solution", jQuery(this).attr("data-solution"));
     jQuery("html, body").animate({ scrollTop: jQuery(document).height() }, "slow");
     jQuery(".answer").focus();
+});
+jQuery(".answer").on("keydown", function () {
+   jQuery(this).val(jQuery(this).val.substr(0, jQuery(this).attr("data-current-solution").length))
+    if(jQuery(this).val().toLowerCase() === jQuery(this).attr("data-current-solution")) {
+        jQuery(".question-box").hide();
+        jQuery('html, body').animate({
+            scrollTop: jQuery(".crossboard").offset().top
+        }, "slow");
+    }
 });
